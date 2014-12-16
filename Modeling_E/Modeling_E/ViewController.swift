af4f183d08c86f5a7eb7e11c79ebb36915a2b716
135759A <e135759@ie.u-ryukyu.ac.jp>
2014-12-16 18:07:36 +0900

    Update ViewController.swift

--- Modeling_E/Modeling_E/ViewController.swift
+++ Modeling_E/Modeling_E/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func unwindToTOP(segue: UIStoryboardSegue) {}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
